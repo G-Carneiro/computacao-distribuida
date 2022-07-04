df9e02c0db2f2d9417eb522a58fed9bd0b1bff37
G-Carneiro <carneiro.gabr@gmail.com>
2022-07-04 09:51:51 -0300

    fair scheduling

--- lab04/consumer.py
+++ lab04/consumer.py
@@ -1,8 +1,12 @@
+from time import sleep
+
 from pika import BlockingConnection, ConnectionParameters
 
 
 def callback(ch, method, properties, body):
-    print(f"Received: '{body.decode()}'")
+    message: str = body.decode()
+    print(f"Received: '{message}'")
+    sleep(len(message))
 
 
 queue = "producer_consumer_queue"
@@ -11,7 +15,7 @@ connection = BlockingConnection(ConnectionParameters(host="localhost"))
 channel = connection.channel()
 
 channel.queue_declare(queue=queue)
-
+channel.basic_qos(prefetch_count=1)
 channel.basic_consume(queue=queue,
                       auto_ack=True,
                       on_message_callback=callback)
--- lab04/producer.py
+++ lab04/producer.py
@@ -1,3 +1,5 @@
+from random import randint
+
 from pika import BlockingConnection, ConnectionParameters
 
 connection = BlockingConnection(ConnectionParameters("localhost"))
@@ -7,11 +9,13 @@ queue = "producer_consumer_queue"
 channel.queue_declare(queue=queue)
 
 while True:
-    message = input("Write a message (quit to exit): ")
+    # message = input("Write a message (quit to exit): ")
+    message = "a" * randint(1, 11)
     if (message.lower() == "quit"):
         break
 
     channel.basic_publish(exchange="",
                           routing_key=queue,
                           body=bytes(message, "UTF-8"))
+    print(f"Sent: '{message}'")
 connection.close()
